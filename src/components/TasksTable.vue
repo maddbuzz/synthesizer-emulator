4d5f5ddd22562eab037e712f0503da8ee825c076
Andrey Anisimov <maddbuzz@gmail.com>
2023-07-05 21:27:52 +0800

    filters

--- src/components/TasksTable.vue
+++ src/components/TasksTable.vue
@@ -9,22 +9,38 @@ defineProps({
 </script>
 
 <template>
-  <v-data-table :headers="headers" :items="items" :items-per-page="itemsPerPage" class="elevation-4">
-    <template v-slot:item.priority="{ item }">
-      <v-chip :color="getPriorityColor(item.priority)" dark>
-        {{ getPriorityName(item.priority) }}
-      </v-chip>
-    </template>
-    <template v-slot:item.createdAt="{ item }">
-      {{ getTimeString(item.createdAt) }}
-    </template>
-    <template v-slot:item.taskEndTime="{ item }">
-      {{ getTimeString(item.taskEndTime) }}
-    </template>
-    <template v-slot:item.completedAt="{ item }">
-      {{ getTimeString(item.completedAt) }}
-    </template>
-  </v-data-table>
+  <div>
+    <v-row>
+      <v-col>
+        <div v-for="filter in selectedFilters" v-bind:key="filter.value">
+          <v-text-field :label="filter.text"></v-text-field>
+        </div>
+      </v-col>
+      <v-col>
+        <v-select :items="availableFilters" @change="onChange($event)"></v-select>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-data-table :headers="headers" :items="items" :items-per-page="itemsPerPage" class="elevation-4">
+          <template v-slot:item.priority="{ item }">
+            <v-chip :color="getPriorityColor(item.priority)" dark>
+              {{ getPriorityName(item.priority) }}
+            </v-chip>
+          </template>
+          <template v-slot:item.createdAt="{ item }">
+            {{ getTimeString(item.createdAt) }}
+          </template>
+          <template v-slot:item.taskEndTime="{ item }">
+            {{ getTimeString(item.taskEndTime) }}
+          </template>
+          <template v-slot:item.completedAt="{ item }">
+            {{ getTimeString(item.completedAt) }}
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -41,10 +57,34 @@ export default {
         { text: 'Created at', value: 'createdAt' },
       ].concat(this.additionalHeaders),
       items: this.tasks,
+
+      allFilters: [
+        { header: 'Select filters' },
+        { text: 'Status', value: 'status' },
+        { text: 'Priority', value: 'priority' },
+        { text: 'Created at', value: 'createdAt' },
+        { text: 'Estimated end time', value: 'taskEndTime' },
+      ],
+      selectedFilters: [],
     };
   },
 
+  computed: {
+    availableFilters() {
+      return this.allFilters
+        .filter(({ value }) => (
+          this.selectedFilters
+            .findIndex((f) => f.value === value) === -1
+        ));
+    },
+  },
+
   methods: {
+    onChange(value) {
+      const newFilter = this.allFilters.find((f) => f.value === value);
+      if (!newFilter) throw Error(`onChange can't find value ${value}`);
+      this.selectedFilters.push(newFilter);
+    },
     getPriorityColor(priority) {
       switch (priority) {
         case 1: return 'blue';
